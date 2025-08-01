eb94e14b30d36846e3ab655818dfa6a9934a997c
Julio Medina <developer.medina@outlook.com>
2025-08-01 00:26:01 -0300

    01-08-2025-C2

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -3,20 +3,3 @@ const menuButton = document.getElementById('menu-toggle');
 menuButton.addEventListener('click', () => {
     document.body.classList.toggle('menu-open');
 })
-
-//getKey
-
-console.log("Minha API Key:", API_KEY);
-
-fetch(`https://api.exemplo.com/dados?apikey=${API_KEY}`)
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(err => console.error("Erro:", err));
-
-
-// Mail function
-
-
-(function () {
-    emailjs.init()
-})
